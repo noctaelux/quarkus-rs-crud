cf472c87c9e972319933795633f20b1f3b532b1e
Oscar Calzada <noctaelux@gmail.com>
2022-11-17 14:40:17 -0600

    quarkus rest service poc

---
+++ src/test/java/com/example/ExampleResourceTest.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class ExampleResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello from RESTEasy Reactive"));
+    }
+
+    @Test
+    public void testGreetingEndpoint(){
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .pathParam("name",uuid)
+                .when().get("/hello/greeting/{name}")
+                .then()
+                    .statusCode(200)
+                .body(is("hello " + uuid));
+    }
+
+}
